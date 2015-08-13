013fac65ab18478366718c563c61acfc5cfc35b3
Corrie Smith & Brigham Thompson <student@epicodus.com>
2015-08-13 09:36:14 -0700

    adds job spec and method to Contacts class

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,8 +1,9 @@
 class Contacts
 
-  define_method(:initialize) do |first_name, last_name|
+  define_method(:initialize) do |first_name, last_name, job|
     @first_name = first_name
     @last_name = last_name
+    @job = job
   end
 
   define_method(:first_name) do
@@ -11,5 +12,9 @@ class Contacts
 
   define_method(:last_name) do
     @last_name
+  end
+
+  define_method(:job) do
+    @job
   end
 end
--- spec/contacts_spec.rb
+++ spec/contacts_spec.rb
@@ -8,15 +8,22 @@ describe(Contacts) do
 
   describe('#first_name') do
     it("returns first name of the contact") do
-      test_contact = Contacts.new("John", "Smith")
+      test_contact = Contacts.new("John", "Smith", "Programmer")
       expect(test_contact.first_name()).to(eq("John"))
     end
   end
 
   describe('#last_name') do
     it("returns last name of the contact") do
-      test_contact = Contacts.new("John", "Smith")
+      test_contact = Contacts.new("John", "Smith", "Programmer")
       expect(test_contact.last_name()).to(eq("Smith"))
+    end
+  end
+
+  describe('#job') do
+    it("returns job title of contact") do
+      test_contact = Contacts.new("John", "Smith", "Programmer")
+      expect(test_contact.job()).to(eq("Programmer"))
     end
   end
 end
