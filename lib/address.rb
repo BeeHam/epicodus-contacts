740d2a6694e851a8e5cbe14846228054e66f0734
Corrie Smith & Brigham Thompson <student@epicodus.com>
2015-08-13 12:23:00 -0700

    address class added to store address of new contacts

---
+++ lib/address.rb
@@ -0,0 +1,12 @@
+class Address
+  attr_reader(:street, :city, :state, :zip, :type)
+  @@address = []
+
+  define_method(:initialize) do |attributes|
+    @street = attributes.fetch(:street)
+    @city = attributes.fetch(:city)
+    @state = attributes.fetch(:state)
+    @zip = attributes.fetch(:zip)
+    @type = attributes.fetch(:type)
+  end
+end
---
+++ spec/address_spec.rb
@@ -0,0 +1,15 @@
+require('rspec')
+require('contacts')
+require('email')
+require('phone')
+require('address')
+
+describe('Address') do
+
+  describe('#street') do
+    it('returns the street of the address') do
+      test_address = Address.new({:street => '123 Main St', :city =>'Sunnyside', :state => 'OR', :zip => '98155', :type => 'work'})
+      expect(test_address.street()).to(eq('123 Main St'))
+    end
+  end
+end
