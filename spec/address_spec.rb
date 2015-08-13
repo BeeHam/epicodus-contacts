0e0920f89c580c1976bf7687b6f716e20178ab31
Corrie Smith & Brigham Thompson <student@epicodus.com>
2015-08-13 12:26:14 -0700

    add city spec to address class

--- spec/address_spec.rb
+++ spec/address_spec.rb
@@ -12,4 +12,11 @@ describe('Address') do
       expect(test_address.street()).to(eq('123 Main St'))
     end
   end
+
+  describe('#city') do
+    it('returns the city of the address') do
+      test_address = Address.new({:street => '123 Main St', :city =>'Sunnyside', :state => 'OR', :zip => '98155', :type => 'work'})
+      expect(test_address.city()).to(eq('Sunnyside'))
+    end
+  end
 end
