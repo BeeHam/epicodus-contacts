75ea1eba141a0d0d03ca1e3c644646e3922046b3
Corrie Smith & Brigham Thompson <student@epicodus.com>
2015-08-13 10:20:03 -0700

    Saves type of email address of contact

--- lib/email.rb
+++ lib/email.rb
@@ -1,12 +1,16 @@
 class Email
   @@email = []
 
-  define_method(:initialize) do |address|
+  define_method(:initialize) do |address, type|
     @address = address
-    #@type = type
+    @type = type
   end
 
   define_method(:address) do
     @address
+  end
+
+  define_method(:type) do
+    @type
   end
 end
--- spec/email_spec.rb
+++ spec/email_spec.rb
@@ -6,9 +6,16 @@ describe('Email') do
 
   describe('#address') do
     it("returns the email address of the contact") do
-      test_email = Email.new("johnsmith@123.com")
+      test_email = Email.new("johnsmith@123.com", "work")
       # test_email.save()
       expect(test_email.address()).to(eq("johnsmith@123.com"))
+    end
+  end
+
+  describe('#type') do
+    it("returns the type of email address of the contact") do
+      test_email = Email.new("johnsmith@123.com", "work")
+      expect(test_email.type()).to(eq("work"))
     end
   end
 end
